19e8e6bc73db05cdaec2201af595c04ca3899485
Caio Mercon <cmpc29@hotmail.com.br>
2024-11-20 18:32:56 -0300

    :construction: feat: Protótipos inciais do site, começando a construir a página inicial

---
+++ avaliacao-de-professores/src/app/usuario/deslogado/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const usuarioDeslogado = () => {
+  return (
+    <div>usuarioDeslogado</div>
+  )
+}
+
+export default usuarioDeslogado
---
+++ avaliacao-de-professores/src/app/usuario/logado/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const usuarioLogado = () => {
+  return (
+    <div> usuarioLogado</div>
+  )
+}
+
+export default usuarioLogado
