e43226cbf4bf859deaa7fd374af6bd387fe050a5
Caio Mercon <cmpc29@hotmail.com.br>
2024-11-23 18:55:20 -0300

    :sparkles: feat: Tela Inicial, com usuário deslogado, está pronta. Além disso, header e cartões de professores estão modularizados em components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 import lupa from "/public/lupa.svg"
-import rick from "/public/Rick.svg"
-import HeaderDeslogado from "./components/HeaderDeslogado";
+import HeaderDeslogado from "./components/HeaderDeslogado"
+import CardProf from "./components/CardProf"
 
 export default function Home() {
   return (
@@ -11,8 +11,8 @@ export default function Home() {
         <header><HeaderDeslogado/></header>
         <div>
           <div className="flex">
-            <div className="ml-24 mt-5 text-black text-[36px]">Novos Professores</div>
-            <div className="mr-36 mt-5 ml-auto mb-6 bg-white rounded-[20px] flex">
+            <div className="ml-20 mt-11 text-black text-[36px]">Novos Professores</div>
+            <div className="mr-32 mt-7 ml-auto mb-6 bg-white rounded-[20px] flex">
               <Image src={lupa} alt={lupa} className="w-[35px] h-[35px] ml-6 place-self-center"/>
               <div className="w-[454px] h-[61px] mt-2 ml-15 flex place-items-center">
                 <Link href="/usuario/Login" className=" ml-20 text-[#999797] text-center text-[25px] leading[25.75px]"> Buscar Professor(a)</Link>
@@ -23,26 +23,23 @@ export default function Home() {
 
         <main>
           <div className="grid grid-cols-4 col-auto mt-7 ml-32 mr-32 place-items-center" >
-              <div className="w-80 h-80 rounded-[10px] bg-white flex flex-col place-items-center hover:bg-[#95BBDFE5]/90">
-                <Image src={rick} alt={rick} className="w-60 h-60 mt-5 rounded-3xl"></Image>
-                <div className="w-52 h-10 mt-2 text-2xl text-center leading-6 font-normal">Rick Sanchez</div>
-                <div className="w-52 h-10 mb-3 text-lg text-center leading-5 break-words text-[#B2B0B0]">Segurança Computacional</div>
-              </div>
-              <div className="w-80 h-80 rounded-[10px] bg-white flex flex-col place-items-center">
-                <Image src={rick} alt={rick} className="w-60 h-60 mt-5 rounded-3xl"></Image>
-                <div className="w-52 h-10 mt-2 text-2xl text-center leading-6 font-normal">Rick Sanchez</div>
-                <div className="w-52 h-10 mb-3 text-lg text-center leading-5 break-words text-[#B2B0B0]">Segurança Computacional</div>
-              </div>
-              <div className="w-80 h-80 rounded-[10px] bg-white flex flex-col place-items-center">
-                <Image src={rick} alt={rick} className="w-60 h-60 mt-5 rounded-3xl"></Image>
-                <div className="w-52 h-10 mt-2 text-2xl text-center leading-6 font-normal">Rick Sanchez</div>
-                <div className="w-52 h-10 mb-3 text-lg text-center leading-5 break-words text-[#B2B0B0]">Segurança Computacional</div>
-              </div>
-              <div className="w-80 h-80 rounded-[10px] bg-white flex flex-col place-items-center">
-                <Image src={rick} alt={rick} className="w-60 h-60 mt-5 rounded-3xl"></Image>
-                <div className="w-52 h-10 mt-2 text-2xl text-center leading-6 font-normal">Rick Sanchez</div>
-                <div className="w-52 h-10 mb-3 text-lg text-center leading-5 break-words text-[#B2B0B0]">Segurança Computacional</div>
-              </div>
+            <CardProf/>
+            <CardProf/>
+            <CardProf/>
+            <CardProf/>
+          </div>
+          <div className="bg-black h-1 mx-36 my-11"></div>
+          <div className="flex">
+            <div className="ml-20 mb-5 text-black text-[36px]">Todos os Professores</div>
+            <Link href="/usuario/Login" className="bg-[#00ABED] w-36 h-11 ml-auto mr-36 border border-white rounded-xl shadow-md text-white text-center text-[30px]">
+            Ordenar
+            </Link>
+          </div>
+          <div className="grid grid-cols-4 col-auto mt-7 ml-32 mr-32 place-items-center" >
+            <CardProf/>
+            <CardProf/>
+            <CardProf/>
+            <CardProf/>
           </div>
         </main>
       </div>
@@ -52,17 +49,9 @@ export default function Home() {
 
 //#95BBDFE5
 // font-family: Questrial;
-// font-size: 30px;
+// font-size: 25px;
 // font-weight: 400;
-// line-height: 30.9px;
-// text-align: center;
-// text-underline-position: from-font;
-// text-decoration-skip-ink: none;
-
-// font-family: Questrial;
-// font-size: 23px;
-// font-weight: 400;
-// line-height: 23.69px;
+// line-height: 25.75px;
 // text-align: center;
 // text-underline-position: from-font;
 // text-decoration-skip-ink: none;
--- src/app/usuario/logado/page.tsx
+++ src/app/usuario/logado/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import HeaderDeslogado from '@/app/components/HeaderDeslogado'
+import CardProf from '@/app/components/CardProf'
 
 const usuarioLogado = () => {
   return (
-    <div> usuarioLogado</div>
+    <>
+    <header><HeaderDeslogado/></header>
+    <body><CardProf/></body>
+    </>
   )
 }
 
 export default usuarioLogado
+
+
