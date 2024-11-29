9a5db3dbe9c27091485224656361aad9511cd0db
Caio Mercon <cmpc29@hotmail.com.br>
2024-11-29 15:23:24 -0300

    :lipstick: feat: Aplicação da fonte onde faltava e resolução de pequenas insconsistências no código

--- src/app/components/HeaderLogado.tsx
+++ src/app/components/HeaderLogado.tsx
@@ -8,10 +8,10 @@ import sino from "/public/sino.svg"
 const HeaderLogado = () => {
   return (
     <header>
-        <div className="w-full h-[99px] bg-[#A4FED3] flex place-items-center">
+        <div className="w-full h-24 bg-[#A4FED3] flex place-items-center">
           <Image src={unb} alt={unb} className="w-24 h-12 ml-9"/>
           <button className="w-11 h-11 ml-auto"><Image src={sino} alt={sino}></Image></button>
-          <button className="w-16 h-16 ml-14 mr-7"><Image src={morty} alt={morty} className='rounded-full' ></Image></button>
+          <button className="w-16 h-16 ml-10 mr-7"><Image src={morty} alt={morty} className='rounded-full' ></Image></button>
           <button className='w-10 h-10 mr-12'><Image src={deslogar} alt={deslogar} ></Image></button>
           
         </div>
