72a6106e6a41827cfbeb7def3e597786df82c460
João Guilherme <76461118+joaogui745@users.noreply.github.com.>
2024-12-11 11:42:26 -0300

    :sparkles: Feat: Tela de cadastro de usuário

--- src/app/cadastro-aluno/page.tsx
+++ src/app/cadastro-aluno/page.tsx
@@ -9,19 +9,24 @@ import * as Yup from 'yup';
 const validationSchema = Yup.object().shape({
   nome: Yup.string().required('Nome inválido'),
   email: Yup.string().email('Email Inválido').required('Email Inválido'),
-  senha: Yup.string().length(10, 'Senha muito Curta').required('Senha Inválida'),
+  senha: Yup.string().min(10, 'Senha muito Curta').required('Senha Inválida'),
+  confirme_senha: Yup.string().oneOf([Yup.ref("senha")] , "As senhas não coincidem").required("Senha Inválida"),
   curso: Yup.string().required('Curso inválido'),
   departamento: Yup.string().required('Departamento Inválida')
 });
 
-const entradas = ["nome", "email", "senha", "curso", "departamento"];
+const entradas = ["nome", "email", "senha", "confirme_senha", "curso", "departamento"];
+
+// Verifica se é um input de senha
+const ehsenha = (entrada : string) : string => ["senha", "confirme_senha"].includes(entrada) ? "password" : "text" 
 
 const initialValues = {
-  nome: "Nome",
-  email: "E-mail",
-  senha: "Senha",
-  curso: "Curso",
-  departamento: "Departamento"
+  nome: "",
+  email: "",
+  senha: "",
+  confirme_senha: "",
+  curso: "",
+  departamento: ""
 };
 
 const onSubmit = (values: Object) => {
@@ -47,11 +52,11 @@ const CadastroAluno = () => {
               <Form className='flex flex-col gap-5'>
               {entradas.map((entrada) =>(
               <div key={entrada}>
-                <Field type="text" name={entrada} className="inputs"/>
+                <Field type={ehsenha(entrada)} name={entrada} placeholder={entrada.replace("_", " ")} className="inputs"/>
                 <ErrorMessage name={entrada} className='text-red-600' component="div" />
               </div>
               ))}
-              <button type="submit" className='bg-[#A4FED3] w-1/3 m-auto p-2 rounded-lg border-blue-600'> Criar Conta </button>
+              <button type="submit" className='bg-[#A4FED3] w-fit  m-auto p-2 pl-4 pr-4 rounded-lg'> Criar Conta </button>
               </Form>
               </Formik>
             </div>
