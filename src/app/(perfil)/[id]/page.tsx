205380c55be034d04d13172f4be2f4ff22c34194
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2024-12-15 23:12:30 -0300

    :stop_sign: feat: formik e toast na tela de login

---
+++ src/app/(perfil)/[id]/page.tsx
@@ -0,0 +1,10 @@
+// import { useState } from "react"
+
+ const Page = () => {
+
+
+//    const[user, setUser] = useState<User | null>(null)
+//     const { id } = useParams()
+//    const router = useRouter()
+
+}
